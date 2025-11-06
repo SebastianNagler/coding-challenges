40b0a38c19fb69209b2638651ca664b03a732def
SebastianNagler <sebastian_nagler@icloud.com>
2025-11-06 18:05:29 +0100

    Time: 960 ms (44.40%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,4 +1,4 @@
-SELECT contest_id, ROUND(100 * COUNT(*)/ (SELECT COUNT(*) FROM Users), 2) AS percentage
-FROM Register
+SELECT contest_id, ROUND(100 * COUNT(*)/ NumUsersT.NumUsersC, 2) AS percentage
+FROM Register CROSS JOIN (SELECT COUNT(*) AS NumUsersC FROM Users) AS NumUsersT
 GROUP BY contest_id
 ORDER BY percentage DESC, contest_id
