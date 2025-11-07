9e353ae0e79e6fdc29f1ff5bda36edd0208ecf52
SebastianNagler <sebastian_nagler@icloud.com>
2025-11-07 15:27:53 +0100

    Time: 443 ms (10.45%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,3 +1,3 @@
-SELECT query_name, ROUND(AVG(rating/position), 2) AS quality, ROUND(100 * SUM(ROUND(1 - (rating/5)))/ COUNT(*), 2) AS poor_query_percentage
+SELECT query_name, ROUND(AVG(rating/position), 2) AS quality, ROUND(100 * AVG(ROUND(1 - (rating/5))), 2) AS poor_query_percentage
 FROM Queries
 GROUP BY query_name
