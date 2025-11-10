ad52037056df368ef59deea16f460c4efad41f49
SebastianNagler <sebastian_nagler@icloud.com>
2025-11-10 12:38:32 +0100

    Time: 428 ms (89.49%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,4 +1,4 @@
 SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
 FROM Activity
 GROUP BY activity_date
-HAVING DATEDIFF('2019-07-27', activity_date) BETWEEN 0 AND 29
+HAVING activity_date BETWEEN '2019-06-28' AND '2019-07-27'
