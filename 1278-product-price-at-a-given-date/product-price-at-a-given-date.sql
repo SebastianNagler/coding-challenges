88382f6089229bc54e345594e3e09dfd6a010838
SebastianNagler <sebastian_nagler@icloud.com>
2025-11-13 10:24:49 +0100

    Time: 467 ms (54.15%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -2,7 +2,7 @@ SELECT DISTINCT
     p1.product_id, 
     (
         CASE WHEN 
-            p2.change_date IS NULL 
+            p2.max_change_date IS NULL 
         THEN 
             10 
         ELSE 
@@ -14,16 +14,17 @@ FROM
     LEFT JOIN
     (
         SELECT 
-            product_id, MAX(change_date) AS change_date
+            product_id, MAX(change_date) AS max_change_date
         FROM 
             Products 
         WHERE 
-            DATEDIFF('2019-08-16', change_date) >= 0 
+            change_date <= '2019-08-16'
         GROUP BY 
             product_id
     ) AS p2
     ON (p1.product_id = p2.product_id)
+
 WHERE 
-    p1.change_date = p2.change_date 
+    p1.change_date = p2.max_change_date 
     OR 
-    p2.change_date IS NULL
+    p2.max_change_date IS NULL
