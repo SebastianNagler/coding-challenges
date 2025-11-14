6c6fda35fa8d2192794c835e52fbc98e35b48fef
SebastianNagler <sebastian_nagler@icloud.com>
2025-11-14 18:57:33 +0100

    Time: 25 ms (60.75%) | Memory: 18.4 MB (98.17%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,15 +1,13 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        reachable = {0}
-        queue = [0]
+        highest = 0
         len_nums = len(nums)
-        while queue:
-            i = queue[0]
-            if i == len_nums - 1:
-                        return True
-            del queue[0]
-            for j in range(min(len_nums - 1, i + nums[i]), i, -1):
-                if j not in reachable:
-                    queue.append(j)
-                    reachable.add(j)
-        return False
+        for num_index in range(len_nums):
+            if num_index >= len_nums - 1:
+                return True
+            num = nums[num_index]
+            if num_index + num > highest:
+                highest = num_index + num
+            elif num_index == highest:
+                return False
+        return True
