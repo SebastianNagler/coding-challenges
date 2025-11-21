e085a50649d90e296de6eeb57452fdb25f613389
SebastianNagler <sebastian_nagler@icloud.com>
2025-11-21 20:19:51 +0100

    Time: 165 ms (41.99%) | Memory: 18.5 MB (16.22%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -6,8 +6,15 @@ class Solution:
         if len_s < len_p:
             return indices
         p_counter = Counter(p)
+        s_counter = Counter(s[0:len_p - 1])
         for i in range(len_s - len_p + 1):
+            if i != 0:
+                s_counter[s[i - 1]] -= 1
+            if s[i + len_p - 1] in s_counter:
+                s_counter[s[i + len_p - 1]] += 1
+            else:
+                s_counter[s[i + len_p - 1]] = 1
             if s[i] in p_counter:
-                if Counter(s[i:i + len_p]) == p_counter:
+                if s_counter == p_counter:
                     indices.append(i)
         return indices
