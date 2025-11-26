1cf42f401612cd9f3ba437af59d91fda73523c55
SebastianNagler <sebastian_nagler@icloud.com>
2025-11-26 17:31:31 +0100

    Time: 236 ms (32.48%) | Memory: 45.4 MB (5.39%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -3,12 +3,14 @@ class Solution:
         n = len(position)
         pos_speed = zip(position, speed)
         pos_speed = [[i, j] for i, j in sorted(pos_speed, key=lambda x: x[0], reverse=True)]
-        stack = []
+        extrap_time = None
+        num_fleets = 0
         for i in range(n):
             if i + 1 < n and pos_speed[i][0] == pos_speed[i+1][0]:
                 pos_speed[i + 1][1] = min(pos_speed[i + 1][1], pos_speed[i][1])
                 continue
-            extrap_time = (target - pos_speed[i][0]) / pos_speed[i][1]
-            if (not stack) or extrap_time > stack[-1]:
-                stack.append(extrap_time)
-        return len(stack)
+            time = (target - pos_speed[i][0]) / pos_speed[i][1]
+            if extrap_time == None or time > extrap_time:
+                extrap_time = time
+                num_fleets += 1
+        return num_fleets
