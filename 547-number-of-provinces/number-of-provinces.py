e0a5f90d9b55d9756b56cab7f05449721977a4cd
SebastianNagler <sebastian_nagler@icloud.com>
2025-11-29 19:49:47 +0100

    Time: 15 ms (16.09%) | Memory: 18.9 MB (70.17%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        self.num_provinces = n
+        self.parent = list(range(n))
+        self.size = [1] * n
+        for row in range(n):
+            for column in range(n):
+                if isConnected[row][column]:
+                    self.union(row, column)
+        return self.num_provinces
+
+        
+    def findRoot(self, node_idx):
+        node_idx_copy = node_idx
+        while node_idx != self.parent[node_idx]:
+            node_idx = self.parent[node_idx]
+        while node_idx != node_idx_copy:
+            self.parent[node_idx_copy] = node_idx
+            node_idx_copy = self.parent[node_idx_copy]
+        return node_idx
+
+    def union(self, node1, node2):
+        node1, node2 = self.findRoot(node1), self.findRoot(node2)
+        if node1 != node2:
+            if self.size[node1] < self.size[node2]:
+                node1, node2 = node2, node1
+            self.parent[node2] = node1
+            self.num_provinces -= 1
+            self.size[node1] += self.size[node2]
