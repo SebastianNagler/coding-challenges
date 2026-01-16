4a11389d7a73e24294504585ccc843d062a4cede
SebastianNagler <sebastian_nagler@icloud.com>
2026-01-16 12:33:52 +0100

    Time: 223 ms (11.41%) | Memory: 20 MB (44.79%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,17 +1,18 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        if target > sum(nums) or target < -sum(nums):
+        sum_nums = sum(nums)
+        if target > sum_nums or target < -sum_nums:
             return 0
-        num_rows = target + sum(nums) + 1
+        num_rows = target + sum_nums + 1
         num_cols = len(nums) + 1
-        grid = [[1]] + ([[0] for i in range(num_rows - 1)])
+        first_row = [1]
         for i in range(1, num_cols):
-            grid[0].append(int(grid[0][-1]))
+            first_row.append(int(first_row[-1]))
             if nums[i - 1] == 0:
-                grid[0][-1] *= 2
+                first_row[-1] *= 2
+        grid = [first_row] + [[0 for i in range(num_cols)] for i in range(num_rows - 1)]
         for row in range(1, num_rows):
             for col in range(1, num_cols):
-                grid[row].append(0)
                 grid[row][col] += grid[row][col - 1]
                 if row - (2 * nums[col - 1]) >= 0:
                     grid[row][col] += grid[row - (2 * nums[col - 1])][col - 1]
