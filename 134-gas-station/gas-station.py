579b89b9494482338d3780f2e9b73be23fbda2ba
SebastianNagler <sebastian_nagler@icloud.com>
2026-01-22 11:16:17 +0100

    Time: 43 ms (14.37%) | Memory: 28.2 MB (5.16%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,8 +1,6 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         gas[0] -= cost[0]
-        if len(gas) == 1:
-            return -1 if gas[0] < 0 else 0
         min_entry_idx = (gas[0], 0)
         for i in range(1, len(gas)):
             gas[i] += gas[i-1] - cost[i]
@@ -10,5 +8,4 @@ class Solution:
                 min_entry_idx = (gas[i], i)
         if gas[-1] < 0:
             return -1
-        res = min_entry_idx[1] + 1
-        return 0 if res == len(gas) else res
+        return 0 if min_entry_idx[1] + 1 == len(gas) else min_entry_idx[1] + 1
