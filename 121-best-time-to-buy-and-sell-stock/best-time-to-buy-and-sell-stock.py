f2a4f548d67e188b20d2c971dbb58b38370df636
SebastianNagler <sebastian_nagler@icloud.com>
2026-08-15 13:15:05 +0200

    Time: 110 ms (5.41%) | Memory: 42.8 MB (7.21%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,8 +1,8 @@
+import numpy as np
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         cum_min = list(itertools.accumulate(prices, func=min))
-        max_profit = float("-inf")
-        for i in range(len(prices)-1):
-            max_profit = max(max_profit, prices[i+1] - cum_min[i])
+        max_profit = max(np.array(prices) - np.array(cum_min))
 
-        return max_profit if max_profit > 0 else 0
+        return int(max_profit) if max_profit > 0 else 0
