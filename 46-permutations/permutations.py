053840d9cc814a38b1507ed26e67d45d2cdee887
SebastianNagler <sebastian_nagler@icloud.com>
2026-08-23 15:10:32 +0200

    Time: 3 ms (31.46%) | Memory: 19.6 MB (9.83%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,18 +1,20 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        if len(nums) == 1:
-            return [nums]
-        """
-        perms_per_i = 1
-        for i in range(2, len(nums)):
-            perms_per_i *= i
-        """
-        for i in range(len(nums)):
-            rest = nums[:i] + nums[i+1:]
-            rest_perms = self.permute(rest)
-            for perm in rest_perms:
-                perm.append(nums[i])
-            res += rest_perms
+        path = []
+        is_remaining = [True] * len(nums)
 
+        def backtrack():
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            for i in range(len(nums)):
+                if not is_remaining[i]:    # pruning goes here
+                    continue
+                path.append(nums[i])      # choose
+                is_remaining[i] = False
+                backtrack()    # explore
+                is_remaining[i] = True
+                path.pop()               # unchoose
+        backtrack()
         return res
