73b6da5200018f5c80f5d764088598890870c3de
SebastianNagler <sebastian_nagler@icloud.com>
2026-08-23 14:20:06 +0200

    Time: 2 ms (36.89%) | Memory: 19.4 MB (66.73%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,18 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def recursivePermute(nums):
-            len_nums = len(nums)
-            if len_nums == 1:
-                return [nums[:]]
-            perms = [item[:] for item in recursivePermute(nums[:-1]) for _ in range(len_nums)]
-            new_num = nums[-1]
-            i = 0
-            for perm in perms:
-                perm.insert(i, new_num)
-                i += 1
-                if i >= len_nums:
-                    i -= len_nums
+        res = []
+        if len(nums) == 1:
+            return [nums]
+        """
+        perms_per_i = 1
+        for i in range(2, len(nums)):
+            perms_per_i *= i
+        """
+        for i in range(len(nums)):
+            rest = nums[:i] + nums[i+1:]
+            rest_perms = self.permute(rest)
+            for perm in rest_perms:
+                perm.append(nums[i])
+            res += rest_perms
 
-            return perms
-                
-        return recursivePermute(nums)
+        return res
